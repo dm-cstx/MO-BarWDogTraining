7ed0e2d8e5f1abf9927ebe5a27482149e360d036
dm-cstx <dana@morningowls.com>
2021-07-30 18:38:20 -0500

    Services list page foundation in place

---
+++ taxonomy-service_type-service.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+<div id="servicesPage">
+    <div class="row container serviceRowLeft align-items-center">
+        <div class="services col">
+            <h1>Title of Service</h1>
+            <p>
+                Lorem ipsum dolor sit amet adipiscing bibendum sem orci tempus aliquet gravida, orci amet iaculis aptent blandit quam accumsan donec in facilisis.
+            </p>
+            <a>Learn More</a>
+        </div>
+        <div class="servicesImg col">
+            <img src="/wp-content/themes/MO-BarWDogTraining/assets/img/BarWlogo.jpg" alt=""  width="500" height="500">
+        </div>
+        <hr>
+    </div>
+    <div class="row container serviceRowRight align-items-center">
+        <div class="servicesImg col">
+            <img src="/wp-content/themes/MO-BarWDogTraining/assets/img/BarWlogo.jpg" alt="" width="500" height="500">
+        </div>
+        <div class="services col">
+            <h1>Title of Service</h1>
+            <p>
+                Lorem ipsum dolor sit amet adipiscing bibendum sem orci tempus aliquet gravida, orci amet iaculis aptent blandit quam accumsan donec in facilisis.
+            </p>
+            <a>Learn More</a>
+        </div>
+        <hr>
+
+    </div>
+</div>
+
+
+<?php get_footer();
