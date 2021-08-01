812cf67ff3e6a92f781e3e6c02a09683956959dc
dm-cstx <dana@morningowls.com>
2021-08-01 11:35:40 -0500

    Product List foundation in place

--- taxonomy-service_type-product.php
+++ taxonomy-service_type-product.php
@@ -1,1 +1,35 @@
-Products
+<?php get_header(); ?>
+
+<div id="productPage">
+    <h1 class="page-header">
+        Products
+    </h1>
+    <section class="row productListSidebar ">
+        <div class="row productRow col-sm-8">
+            <?php 
+                while(have_posts()) {
+                the_post();  ?>
+            <div class="card productCard col-md-8 col-lg-3" >
+                <div >
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('', ['class' => 'card-img-top productImg']); ?></a>
+                </div>
+
+                <div class="card-body productBody">
+                    <h2 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet adipiscing bibendum sem orci tempus aliquet gravida, orci amet.</p>
+                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+            <?php  
+                } ?>
+        </div>
+        <div class="productSidebar col-sm-4">
+            <?php get_template_part('template-parts/sidebar-product'); ?>
+        </div>
+</section>
+    
+    
+</div>
+
+
+<?php get_footer();
---
+++ template-parts/sidebar-product.php
@@ -0,0 +1,25 @@
+<?php  ?>
+
+
+
+    <div id="sidebar-content" class="p-3">
+		<div class="side-title">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/BarWlogo.jpg" alt=""  width="200" height="200">
+		</div>
+		<div class="side-note">
+			<p>
+				Lorem ipsum dolor sit amet adipiscing bibendum sem orci tempus aliquet gravida
+			</p>
+		</div>
+		<div class="pt-5">
+            <h2 class="cat-title">Categories</h2>
+			<?php  
+				wp_list_categories( array (
+					'style' => "",
+					'title_li' => ""
+				));
+			?>
+		</div>
+    </div>
+
+
