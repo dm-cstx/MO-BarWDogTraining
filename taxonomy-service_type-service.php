b918daa8227c1a7d4ab3d00d9e0c2af5d9e1390f
dm-cstx <dana@morningowls.com>
2021-08-10 17:01:56 -0500

    products list page styled

--- taxonomy-service_type-service.php
+++ taxonomy-service_type-service.php
@@ -13,50 +13,58 @@
     </div>
 </div>
 <div id="servicesPage" class="container-fluid">
-<?php 
-if ( have_posts() ) {
-    $count = 0;
-	while ( have_posts() ) {
-        the_post();
-        ++$count;
-        if (($count % 2) == 1) { ?>
-            <div class="row servicesOdd">
-                <div class="col-md-7">
-                    <a href="<?php the_permalink(); ?>"><?php 
-                    the_post_thumbnail();
-                    ?></a>
-                </div>
-                <div class="col-md-5">
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h2>
-                    <?php the_field('price'); ?>
-                    <div class="servicesDetails">
-                        <?php the_field ('details');?>
+    <h1 class="page-header">
+        Services
+    </h1>
+    <?php 
+    if ( have_posts() ) {
+        $count = 0;
+        while ( have_posts() ) {
+            the_post();
+            ++$count;
+            if (($count % 2) == 1) { ?>
+                <div class="servicesOdd">
+                    <div class="row servicesRow">
+                        <div class="col-md-7">
+                            <a href="<?php the_permalink(); ?>"><?php 
+                            the_post_thumbnail();
+                            ?></a>
+                        </div>
+                        <div class="col-md-5">
+                            <h2><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h2>
+                            <?php the_field('price'); ?>
+                            <div class="servicesDetails">
+                                <?php the_field ('details');?>
+                            </div>
+                            <a href="<?php the_permalink(); ?>"><button class="fpCTA ms-3" type="button">Learn&nbsp;More</button></a>
+                        </div>
                     </div>
-                    <a href="<?php the_permalink(); ?>"><button class="fpCTA ms-3" type="button">Learn&nbsp;More</button></a>
+                    
+                </div> 
+                <?php
+            }
+            else {  ?>
+                <div class="row servicesEven">
+                    <div class="row servicesRow">
+                        <div class="col-md-5">
+                            <h2><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h2>
+                            <?php the_field('price'); ?>
+                            <div class="servicesDetails">
+                                <?php the_field ('details');?>
+                            </div>
+                            <a href="<?php the_permalink(); ?>"><button class="fpCTA ms-3" type="button">Learn&nbsp;More</button></a>
+                        </div>
+                        <div class="col-md-7">
+                            <a href="<?php the_permalink(); ?>"><?php 
+                            the_post_thumbnail();
+                            ?></a>
+                        </div> 
+                    </div>
                 </div>
-            </div> 
-            <?php
+                <?php
+            } 
         }
-        else {  ?>
-            <div class="row servicesEven">
-                <div class="col-md-5">
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h2>
-                    <?php the_field('price'); ?>
-                    <div class="servicesDetails">
-                        <?php the_field ('details');?>
-                    </div>
-                    <a href="<?php the_permalink(); ?>"><button class="fpCTA ms-3" type="button">Learn&nbsp;More</button></a>
-                </div>
-                <div class="col-md-7">
-                    <a href="<?php the_permalink(); ?>"><?php 
-                    the_post_thumbnail();
-                    ?></a>
-                </div>                
-            </div>
-            <?php
-        } 
-    }
-}?>
+    }?>
 
 </div>
 
--- template-parts/sidebar-product.php
+++ template-parts/sidebar-product.php
@@ -2,17 +2,22 @@
 
 
 
-    <div id="sidebar-content" class="p-3">
+    <div id="sidebarProduct" class="p-3">
 		<div class="side-title">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/BarWlogo.png" alt=""  width="200" height="200">
 		</div>
-		<div class="side-note">
+		<div class="sideNote">
 			<p>
 				Lorem ipsum dolor sit amet adipiscing bibendum sem orci tempus aliquet gravida
 			</p>
 		</div>
-		<div class="pt-5">
-            <h2 class="cat-title">Categories</h2>
+		<div class="servicesSidebar">
+			<h2 class="cat-title">
+				Services
+			</h2>
+		</div>
+		<div class="productsSidebar pt-5">
+            <h2 class="cat-title">Product Categories</h2>
 			<?php  
 				wp_list_categories( array (
 					'style' => "",
